23b88b61036dd04a2daf6133dfe92e3ec869f938
Shimanshu Chauhan <118818189+ShimanshuChauhan@users.noreply.github.com>
2025-07-29 17:54:42 +0530

    Update catchAsync.js

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,9 +1,5 @@
-export default catchAsync = (fn) => {
-  return (req, res, next) => fn(req, res, next).catch(err => {
-    // Log the error for debugging purposes
-    // console.error('Async Error:', err);
-
-    // Pass the error to the next middleware
-    next(err);
-  });
+const catchAsyc = (fn) =>{
+  return (req,res,next) => fn(req,res,next).catch(next);
 }
+
+export default catchAsync;
